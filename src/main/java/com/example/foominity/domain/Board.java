42f2aec7771fd1e3a39cfd8c282655eda9eda092
KimYouHwan <voddl1664@gmail.com>
2025-05-30 17:12:04 +0900

    feat/board

--- src/main/java/com/example/foominity/domain/Board.java
+++ src/main/java/com/example/foominity/domain/Board.java
@@ -2,14 +2,19 @@ package com.example.foominity.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 @Entity
 public class Board {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
