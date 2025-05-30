24e61f882751a1784ad49f67f8a8d326b609d56e
CheonUng <cjsdnd9712@gmail.com>
2025-05-30 10:00:03 +0900

    첫 업로드

---
+++ src/main/java/com/example/foominity/FoominityApplication.java
@@ -0,0 +1,13 @@
+package com.example.foominity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoominityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoominityApplication.class, args);
+	}
+
+}
