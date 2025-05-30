bf848861db9d9da95609cc60bc571e95ac33c428
KimYouHwan <voddl1664@gmail.com>
2025-05-30 17:38:26 +0900

    Review.java

---
+++ src/main/java/com/example/foominity/domain/Review.java
@@ -0,0 +1,33 @@
+package com.example.foominity.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String category;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(unique = true)
+    private String member_id;
+
+    private float starpoint;
+
+}
