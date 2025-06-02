d50b8448bfc760a00b278529e308bf2b2a90a0f3
KimYouHwan <voddl1664@gmail.com>
2025-06-02 15:50:30 +0900

    자유게시판 Entity

--- src/main/java/com/example/foominity/domain/Board.java
+++ src/main/java/com/example/foominity/domain/Board.java
@@ -1,10 +1,15 @@
 package com.example.foominity.domain;
 
+import java.lang.reflect.Member;
+
+import com.fasterxml.jackson.databind.JsonSerializable.Base;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,13 +22,13 @@ public class Board {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JoinColumn(name = "member_id")
+    private Member memberId;
+
     @Column(nullable = false)
     private String title;
 
     @Column(nullable = false)
     private String content;
-
-    @Column(unique = true)
-    private String username;
 
 }
