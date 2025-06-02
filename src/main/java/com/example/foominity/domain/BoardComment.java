0e824efb516805f9aa2083707facb406dcb0748e
leedonghyeon <0928dlehdgus@gmail.com>
2025-06-02 16:47:02 +0900

    이동현(comment) - 보드코멘트 , 팔로잉 주석

--- src/main/java/com/example/foominity/domain/BoardComment.java
+++ src/main/java/com/example/foominity/domain/BoardComment.java
@@ -1,35 +1,35 @@
-package com.example.foominity.domain;
+// package com.example.foominity.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.FetchType;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.JoinColumn;
+// import jakarta.persistence.ManyToOne;
+// import lombok.Getter;
+// import lombok.NoArgsConstructor;
+// import lombok.ToString;
 
-@ToString
-@NoArgsConstructor
-@Getter
-@Entity
-public class BoardComment {
+// @ToString
+// @NoArgsConstructor
+// @Getter
+// @Entity
+// public class BoardComment {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+// @Id
+// @GeneratedValue(strategy = GenerationType.IDENTITY)
+// private Long id;
 
-    // 유저 닉네임 (멤버)
-    private Member member;
+// // 유저 닉네임 (멤버)
+// private Member member;
 
-    // 댓글 내용
-    private String content;
+// // 댓글 내용
+// private String content;
 
-    // 게시판 아이디
-    @JoinColumn(name = "board_id")
-    @ManyToOne(fetch = FetchType.LAZY)
-    private String board;
+// // 게시판 아이디
+// @JoinColumn(name = "board_id")
+// @ManyToOne(fetch = FetchType.LAZY)
+// private String board;
 
-}
+// }
