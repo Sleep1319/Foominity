cc6d1f62079e03708d94426b15f316940af1cd95
gannniii <kke08kr@gmail.com>
2025-06-02 18:13:45 +0900

    김가은(report) - service 추가

---
+++ src/main/java/com/example/foominity/service/notice/NoticeService.java
@@ -0,0 +1,10 @@
+package com.example.foominity.service.notice;
+
+import org.springframework.stereotype.Service;
+
+import com.example.foominity.repository.notice.NoticeRepository;
+
+@Service
+public class NoticeService {
+
+}
