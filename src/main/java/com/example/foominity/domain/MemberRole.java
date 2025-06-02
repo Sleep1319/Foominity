78334c6e03b77c993636d710ab06f137b2ca61d2
kims0711 <altjd10@gmail.com>
2025-06-02 15:21:39 +0900

    김민성 - Member, MemberRole 엔티티 수정, Role 엔티티 작성

--- src/main/java/com/example/foominity/domain/MemberRole.java
+++ src/main/java/com/example/foominity/domain/MemberRole.java
@@ -1,5 +1,27 @@
 package com.example.foominity.domain;
 
-public enum MemberRole {
-    ADMIN, NORMAL, VIP
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString
+@Entity
+public class MemberRole {
+
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
 }
