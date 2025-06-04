a9cb86a20b0bf9b1691075fe3c2867413f19876e
kims0711 <altjd10@gmail.com>
2025-06-04 18:01:10 +0900

    김민성 - SignService 에 Transactional 어노테이션 작성

--- src/main/java/com/example/foominity/service/sign/SignService.java
+++ src/main/java/com/example/foominity/service/sign/SignService.java
@@ -2,6 +2,7 @@ package com.example.foominity.service.sign;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.foominity.dto.sign.SignUpRequest;
 import com.example.foominity.repository.sign.SignRepository;
@@ -9,13 +10,14 @@ import com.example.foominity.repository.sign.SignRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class SignService {
 
     private final SignRepository signRepository;
     private final PasswordEncoder passwordEncoder;
 
-    // public void signUp(SignUpRequestDTO req){
+    // public void signUp(SignUpRequest req){
     // validateSignUp(req.getEmail(), req.getNickname());
     // req.setPassword(passwordEncoder.encode(req.getPassword()));
     // Role
