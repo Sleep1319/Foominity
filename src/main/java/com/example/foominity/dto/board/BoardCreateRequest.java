fd7e8be8deef15122b76b6734d89a4841b08f899
CheonUng <cjsdnd9712@gmail.com>
2025-06-11 17:54:38 +0900

    서버 실행전 에러 파일 수정 및 주석 처리

--- src/main/java/com/example/foominity/dto/board/BoardCreateRequest.java
+++ src/main/java/com/example/foominity/dto/board/BoardCreateRequest.java
@@ -1,8 +1,7 @@
 //package com.example.foominity.dto.board;
 //
+//import com.example.foominity.domain.board.Board;
 //import com.example.foominity.domain.member.Member;
-//import com.example.reactbootserver.domain.Member;
-//import com.example.reactbootserver.domain.board.Board;
 //import jakarta.validation.constraints.NotNull;
 //import lombok.AllArgsConstructor;
 //import lombok.Data;
@@ -23,6 +22,6 @@
 //    private String content;
 //
 //    public Board toEntity(BoardCreateRequest req, Member member) {
-//        return new Board(req.getTitle(), req.title, member);
+//        return new Board(req.getTitle(), req., member);
 //    }
 //}
