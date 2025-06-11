cbceff361d9a1af6a9a19f76d8b8d267cf96619c
CheonUng <cjsdnd9712@gmail.com>
2025-06-11 10:17:32 +0900

    mui적용 테스트

---
+++ frontend/src/view/Home.jsx
@@ -0,0 +1,23 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import * as React from "react";
+
+function Home(props){
+    return (
+        <AppTheme {...props}>
+            <CssBaseline enableColorScheme />
+            <AppAppBar />
+            <Container
+                maxWidth="lg"
+                component="main"
+                sx={{ display: 'flex', flexDirection: 'column', my: 16, gap: 4 }}
+            >
+                <MainContent />
+                <Latest />
+            </Container>
+            <Footer />
+        </AppTheme>
+    );
+}
+
+export default Home;
