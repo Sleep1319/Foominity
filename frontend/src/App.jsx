4a65b0c8d1a1f5849c7cc059716068f447471378
CheonUng <cjsdnd9712@gmail.com>
2025-06-17 17:32:04 +0900

    라우터 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AppNavbar from './components/AppNavbar'
+import AppRoutes from "@/routes/Routes.jsx";
 import Home from './view/Home.jsx'
 
 function App() {
     return (
         <BrowserRouter>
             <AppNavbar />
-            <Home />
+            <AppRoutes/>
         </BrowserRouter>
     )
 }
