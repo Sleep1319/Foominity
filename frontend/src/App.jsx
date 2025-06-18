5516c62436d842bd7b8171a422ca4b74490f0686
CheonUng <cjsdnd9712@gmail.com>
2025-06-18 20:24:35 +0900

    브라우저 라우터 우선순위 변경

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,14 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppNavBar from "./layouts/AppNavBar.jsx";
 import AppRoutes from "@/routes/Routes.jsx";
 import {UserProvider} from "@/context/UserContext.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
         <UserProvider>
             <AppNavBar />
             <AppRoutes />
         </UserProvider>
-    </BrowserRouter>
   );
 }
 export default App;
