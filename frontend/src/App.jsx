ab5c424d09fb1da1e609a9db0554591d70a0a44a
CheonUng <cjsdnd9712@gmail.com>
2025-06-18 17:19:59 +0900

    로그인 유저 정보 조회기능/ 유저 로그인/비로그인 페이지 이동 제한 기능 구현

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppNavBar from "./components/AppNavBar";
 import AppRoutes from "@/routes/Routes.jsx";
-import Home from "./view/Home.jsx";
+import {UserProvider} from "@/context/UserContext.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <AppNavBar />
-      <AppRoutes />
+        <UserProvider>
+            <AppNavBar />
+            <AppRoutes />
+        </UserProvider>
     </BrowserRouter>
   );
 }
--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -4,17 +4,24 @@ import Home from "../view/Home.jsx";
 import Report from "../view/Report.jsx";
 import Login from "./../view/Login";
 import Register from "./../view/Register";
+import Notice from "@/view/Notice.jsx";
+import ProtectedRoute from "./ProtectedRoute";
 
 function AppRoutes() {
-  return (
+    return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/report" element={<Report />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/report" element={<Report />} />
+        <Route path="/notie" element={<Notice/>} />
+        <Route element={<ProtectedRoute requireAuth={false}/> }>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+        </Route>
+        <Route element={<ProtectedRoute requireAuth={true}/> }>
+        {/* 로그인 필수 페이지 */}
+        </Route>
     </Routes>
-  );
+    );
 }
 
 export default AppRoutes;
