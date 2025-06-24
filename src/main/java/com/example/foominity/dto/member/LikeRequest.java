29ac7dad9fc51b5365f79f7409d3be72ea826080
leedonghyeon <0928dlehdgus@gmail.com>
2025-06-24 14:58:07 +0900

    이동현 - (좋아요, 포인트) 관계설정 변경

--- src/main/java/com/example/foominity/dto/member/LikeRequest.java
+++ src/main/java/com/example/foominity/dto/member/LikeRequest.java
@@ -1,6 +1,6 @@
 package com.example.foominity.dto.member;
 
-import com.example.foominity.domain.board.Review;
+import com.example.foominity.domain.board.ReviewComment;
 import com.example.foominity.domain.member.Like;
 import com.example.foominity.domain.member.Member;
 
@@ -16,8 +16,8 @@ public class LikeRequest {
 
     private boolean liked;
 
-    public Like toEntity(Review review, Member member) {
-        return new Like(review, member);
+    public Like toEntity(ReviewComment reviewComment, Member member) {
+        return new Like(reviewComment, member);
     }
 
 }
