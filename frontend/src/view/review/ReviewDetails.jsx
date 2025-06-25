361ea8e2bc94c14e2375900abe0969f870c1c51c
CheonUng <cjsdnd9712@gmail.com>
2025-06-26 03:30:39 +0900

    댓글 호출 기능구현

--- frontend/src/view/review/ReviewDetails.jsx
+++ frontend/src/view/review/ReviewDetails.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReviewDetail from "../../components/reviewComponents/ReviewDetail.jsx";
+import ReviewCommentForm from "@/components/commentComponents/ReviewCommentForm.jsx";
 
 const ReviewDetails = () => {
   return (
     <div>
       <ReviewDetail />
+        <ReviewCommentForm />
     </div>
   );
 };
