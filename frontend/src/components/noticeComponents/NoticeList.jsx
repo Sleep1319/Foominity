e61fe430329e51e3a6eb4f8d220791977ad219fa
gannniii <kke08kr@gmail.com>
2025-06-27 10:03:15 +0900

    김가은 - 노티스 수정

--- frontend/src/components/noticeComponents/NoticeList.jsx
+++ frontend/src/components/noticeComponents/NoticeList.jsx
@@ -1,15 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {
-  Box,
-  Heading,
-  Flex,
-  Text,
-  SimpleGrid,
-  Card,
-  CardBody,
-  Button,
-  HStack,
-} from "@chakra-ui/react";
+import { Box, Heading, Flex, Text, SimpleGrid, Card, CardBody, Button, HStack } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useUser } from "../../context/UserContext";
@@ -42,7 +32,7 @@ const NoticeList = () => {
   return (
     <Box p={6} maxW="1000px" mx="auto">
       <Flex justify="space-between" mb={6} align="center">
-        {user?.roleName === "ADMIN" && (
+        {user?.roleName === "admin" && (
           <Button colorScheme="blue" size="sm" onClick={() => navigate("/notice/create")}>
             글 작성
           </Button>
@@ -50,45 +40,33 @@ const NoticeList = () => {
       </Flex>
 
       <SimpleGrid spacing={4}>
-  {currentNotices.map((notice) => {
-
-
-    return (
-      <Card
-        key={notice.id}
-        borderRadius="lg"
-        _hover={{ shadow: "md", cursor: "pointer" }}
-        onClick={() => {
-       
-        navigate(`/notice/${notice.id}`);    
-      }}
-      >
-        <CardBody>
-          <Text fontSize="lg" fontWeight="bold">
-            {notice.title}
-          </Text>
-        </CardBody>
-      </Card>
-    );
-  })}
-</SimpleGrid>
+        {currentNotices.map((notice) => {
+          return (
+            <Card
+              key={notice.id}
+              borderRadius="lg"
+              _hover={{ shadow: "md", cursor: "pointer" }}
+              onClick={() => {
+                navigate(`/notice/${notice.id}`);
+              }}
+            >
+              <CardBody>
+                <Text fontSize="lg" fontWeight="bold">
+                  {notice.title}
+                </Text>
+              </CardBody>
+            </Card>
+          );
+        })}
+      </SimpleGrid>
       {/* 페이지네이션 */}
       <HStack spacing={2} justify="center" mt={8}>
-        <Button
-          size="sm"
-          onClick={() => setPage((prev) => Math.max(prev - 1, 0))}
-          isDisabled={page === 0}
-        >
+        <Button size="sm" onClick={() => setPage((prev) => Math.max(prev - 1, 0))} isDisabled={page === 0}>
           이전
         </Button>
 
         {[...Array(totalPages)].map((_, i) => (
-          <Button
-            key={i}
-            size="sm"
-            variant={i === page ? "solid" : "outline"}
-            onClick={() => setPage(i)}
-          >
+          <Button key={i} size="sm" variant={i === page ? "solid" : "outline"} onClick={() => setPage(i)}>
             {i + 1}
           </Button>
         ))}
