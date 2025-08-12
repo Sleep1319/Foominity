571c8d7ec3af714da9ec2231704f617769115b4d
CheonUng <cjsdnd9712@gmail.com>
2025-08-12 13:17:50 +0900

    리덕스 설정 변경

--- frontend/src/redux/useUser.js
+++ frontend/src/redux/useUser.js
@@ -1,12 +1,28 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector, shallowEqual } from "react-redux";
 import { updateUser, logout } from "./userSlice";
 
 export function useUser() {
     const dispatch = useDispatch();
-    // Redux의 user slice를 가져와서
-    const u = useSelector((s) => s.user);
 
-    // 예전 UserContext와 동일한 형태로 맞춰서 반환
+    // 필요한 필드만 뽑아서 shallowEqual로 리렌더 최소화
+    const u = useSelector(
+        (s) => ({
+            id: s.user.id,
+            email: s.user.email,
+            username: s.user.username,
+            nickname: s.user.nickname,
+            role: s.user.role,
+            avatar: s.user.avatar,
+            socialType: s.user.socialType,
+            createdAt: s.user.createdAt,
+            isLoading: s.user.isLoading,
+            hydrated: s.user.hydrated,
+            isLoggingOut: s.user.isLoggingOut,
+        }),
+        shallowEqual
+    );
+
+    // 예전 UserContext 형태로 어댑팅
     const state = {
         id: u.id,
         memberId: u.id,
@@ -20,10 +36,10 @@ export function useUser() {
     };
 
     return {
-        state,                    // ✅ 예전처럼 state.id 로 접근 가능
-        isLoading: u.isLoading,
+        state,                                 // state.id / state.roleName 그대로 사용
+        isLoading: !u.hydrated && u.isLoading, // 첫 수화 전에만 로딩 표시
         isLoggingOut: u.isLoggingOut,
-        updateUser: (patch) => dispatch(updateUser(patch)),
+        updateUser: (patch) => dispatch(updateUser(patch)), // 미리보기 즉시 반영
         logout: () => dispatch(logout()),
     };
 }
